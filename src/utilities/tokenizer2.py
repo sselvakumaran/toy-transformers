bbc89e4a0f8cfd56a86b7dae8ffba185a7c4776c
sselvakumaran <srimans@umd.edu>
2025-11-11 16:17:38 -0500

    initial tokenizer done, mid-second iteration tokenizer half done

---
+++ src/utilities/tokenizer2.py
@@ -0,0 +1,126 @@
+# BPE CHANGES
+# 1. instead of tokens / strings use ints 
+# 2. use numpy
+# 3. use Counter / counter.top_k(1) instead
+# 4. create dict pair -> set(indicies), only update neighboring pair counts
+# 5. updating neighboring pair counts:
+#      using dict from (4), get all indicies, search left and right for neighboring tokens
+#      (replace deleted tokens with -1 for convenience, occasionally clear all -1s)
+#      update counter and 
+
+from typing import Dict, Callable
+import heapq
+from collections import Counter, namedtuple, defaultdict
+import numpy as np
+
+TokenDictionary = namedtuple('TokenDictionary', 
+	['token_set', 'idx_to_token', 'token_to_idx']
+)
+
+class _LazyHeap():
+	def __init__(self, 
+		refresh: Callable,
+		is_valid: Callable,
+		heap = None,
+		amortize_period = 50,
+	):
+		self.heap = heap if heap else []
+		heapq.heapify(self.heap)
+
+		self.refresh = refresh
+		self.is_valid = is_valid
+		self.count = 0
+		self.limit = amortize_period
+	
+	def __bool__(self):
+		return len(self.heap) > 0
+
+	def __len__(self):
+		return len(self.heap)
+
+	def pop(self):
+		item = heapq.heappop(self.heap)
+		while not self.is_valid(*item):
+			self.count += 1
+			item = heapq.heappop(self.heap)
+
+			if self.count >= self.limit:
+				self.count = 0
+				self.heap = self.refresh()
+				heapq.heapify(self.heap)
+		return item
+		
+	def push(self, item):
+		heapq.heappush(self.heap, item)
+
+class _TokenList:
+	def __init__(self, text: np.ndarray):
+		N = len(text)
+		self.tokens = np.copy(text)
+		self.prev = np.arange(-1, N - 1)
+		self.next = np.arange(1, N + 1)
+
+		self.pairs = defaultdict(list)
+		self.counts = defaultdict(int)
+		for id in range(N - 1):
+			pair = (text[id], text[id+1])
+			self.pairs[pair].append(id)
+			self.counts[pair] += 1
+	
+	def get_heap(self):
+		return [(-count, pair) for pair, count in self.counts]
+	
+	def get_count(self, pair):
+		return self.counts[pair]
+	
+	def merge(t1, t2, t_new):
+		# iterate through all indices with (t1, t2)
+		# for all i (signifies where t1 is):
+		#   j <- next[i]
+		#   token[i] <- t_new
+		#   next[i] <- next[next[i]]
+		pass
+
+def create_td(token_set: list) -> TokenDictionary:
+	idx_to_token = {i: t for i, t in enumerate(token_set)}
+	token_to_idx = {t: i for i, t in enumerate(token_set)}
+	return TokenDictionary(token_set, idx_to_token, token_to_idx)
+
+def create_tokenizer(data: str, num_tokens: int):
+	token_set, id_to_token, token_to_id = create_td(sorted(set(data)))
+	text = np.array([token_to_id[token] for token in data], dtype=np.int16)
+
+	pairs = np.stack([text[:-1], text[1:]], axis=1)
+	unique_pairs, counts = np.unique(pairs, axis=0, return_counts=True)
+	pair_counts = dict(zip(map(tuple, unique_pairs), counts))
+
+	pair_positions = defaultdict(set)
+	for i in range(len(text) - 1):
+		pair_positions[(text[i], text[i+1])].add(i)
+
+	heap = _LazyHeap(
+		refresh = lambda: [(-c, pair) for pair, c in pair_counts.items() if c > 0],
+		is_valid = lambda neg_c, pair: pair_counts[pair] == -neg_c,
+		heap = [(-c, p) for p, c in pair_counts.items()]
+	)
+
+	token_count = len(token_set)
+	while token_count < num_tokens and heap:
+		_, (t1, t2) = heap.pop()
+		new_token = token_count + 1
+		id_to_token[new_token] = id_to_token[t1] + id_to_token[t2]
+		# replace (a, b)
+		
+		# update text
+
+		# update pair_counts
+
+		token_count += 1
+
+	return None
+
+
+
+if __name__ == '__main__':
+	data = "banana"
+	td = create_tokenizer(data, num_tokens=10)
