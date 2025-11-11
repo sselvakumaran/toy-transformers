6557ce9ff2c95979ee12b6350b4cf079ef346d1a
sselvakumaran <srimans@umd.edu>
2025-11-11 16:18:05 -0500

    some plotter work

---
+++ src/utilities/loss_plot.py
@@ -0,0 +1,24 @@
+# NEW VERSION
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class LossTracker:
+	# initialize settings
+	def __init__(self):
+		pass
+	
+	# Add singular value to data tracker
+	def add_points(self):
+		pass
+
+	# Add batch of values to data tracker 
+	def add_points_batch(self):
+		pass
+
+	# Return figure to print
+	def get_updated_figure(self):
+		pass
+
+	
+
